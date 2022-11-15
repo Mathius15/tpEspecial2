0219a364654b3ac29cd5c9f1491bbde2ee32dba1
Matias Moraga <matimoraga10@gmail.com>
2022-11-15 18:41:39 -0300

    Actualizo base de datos

--- database/reviews .sql
+++ database/reviews .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2022 at 02:52 AM
+-- Generation Time: Nov 15, 2022 at 10:40 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -33,7 +33,7 @@ CREATE TABLE `episodios` (
   `Duracion` int(11) NOT NULL,
   `Temporada` int(11) NOT NULL,
   `Descripcion` text NOT NULL,
-  `Puntuacion` int(11) NOT NULL,
+  `Puntuacion` float(11,0) NOT NULL,
   `Serie` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -42,11 +42,13 @@ CREATE TABLE `episodios` (
 --
 
 INSERT INTO `episodios` (`ID_episodio`, `Titulo`, `Duracion`, `Temporada`, `Descripcion`, `Puntuacion`, `Serie`) VALUES
-(30, 'Geheimnisse', 55, 1, 'The small German town of Winden is shaken by the disappearance of a teenage boy. While the townsfolk are occupied with secrets of their own, at nightfall a group of teenagers attempts to recover something the missing boy may have left behind.', 8, 'Dark'),
-(31, 'Lügen', 44, 1, ' When a grim discovery leaves the police baffled, Ulrich seeks a search warrant for the power plant. A mysterious stranger checks into the hotel.', 8, 'Dark'),
-(32, 'Gestern und heute', 45, 1, 'It\'s 1986, and Ulrich\'s brother, Mads, has been missing for a month. Confusion reigns as past and present intertwine.', 9, 'Dark'),
 (33, 'Be Right Back', 48, 2, ' After learning about a new service that lets people stay in touch with the deceased, a lonely, grieving Martha reconnects with her late lover.', 8, 'Black Mirror'),
-(34, 'USS Callister', 76, 4, 'Capt. Robert Daly presides over his crew with wisdom and courage. But a new recruit will soon discover nothing on this spaceship is what it seems.', 8, 'Black Mirror');
+(34, 'USS Callister', 76, 4, 'Capt. Robert Daly presides over his crew with wisdom and courage. But a new recruit will soon discover nothing on this spaceship is what it seems.', 8, 'Black Mirror'),
+(46, 'titulasoo', 3, 3, 'desc', 3, 'Dark'),
+(47, 'dsadas', 3, 3, 'dsadasd', 5, 'Black Mirror'),
+(48, 'dddddddadw', 55, 4, 'ddas3hht', 2, 'Black Mirror'),
+(49, 'fhythnuy', 54, 6, 'ujygdfgf', 4, 'Rick_And_Morty'),
+(50, 'kopñjgf', 21, 1, 'jufgdasdqw', 2, 'Rick_And_Morty');
 
 -- --------------------------------------------------------
 
@@ -69,9 +71,12 @@ CREATE TABLE `series` (
 --
 
 INSERT INTO `series` (`ID_serie`, `Nombre`, `Descripcion`, `Puntuacion`, `Creadores`, `Genero`, `img`) VALUES
-(24, 'Dark', 'A family saga with a supernatural twist, set in a German town where the disappearance of two young children exposes the relationships among four families.', 8.7, ' Baran bo Odar - Jantje Friese', 'Drama', 'img/serie/6349b43388ccc.jpg'),
-(25, 'South Park', 'Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.', 8.7, 'Trey Parker - Matt Stone - Brian Graden', 'Comedy', 'img/serie/6349bb8bac8e9.jpg'),
-(26, 'Black Mirror', 'An anthology series exploring a twisted, high-tech multiverse where humanity\'s greatest innovations and darkest instincts collide.', 8.8, ' Charlie Brooker', 'Drama - Mystery', 'img/serie/6349bbfcdf9d2.jpg');
+(25, 'South_Park', 'Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.', 8.7, 'Trey Parker - Matt Stone - Brian Graden', 'Comedy', 'img/serie/6349bb8bac8e9.jpg'),
+(26, 'Black Mirror', 'An anthology series exploring a twisted, high-tech multiverse where humanity\'s greatest innovations and darkest instincts collide.', 8.8, ' Charlie Brooker', 'Drama - Mystery', 'img/serie/6349bbfcdf9d2.jpg'),
+(30, 'Dark', 'descripcion', 2, 'Trey Parker - Matt Stone - Brian Graden', 'dwq', 'img/serie/636aa98c2bf81.jpg'),
+(32, 'cccc', 'dasd', 3, 'dasdas', 'dsadas', 'img/serie/636bb7c1b750c.png'),
+(34, 'Rick_And_Morty', 'las aventudsada', 9.9, 'justin roiland', 'cyfi', ''),
+(35, 'Rick_And_Morty', 'las aventudsada', 9.9, 'justin roiland', 'cyfi', '');
 
 -- --------------------------------------------------------
 
@@ -124,13 +129,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT for table `episodios`
 --
 ALTER TABLE `episodios`
-  MODIFY `ID_episodio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `ID_episodio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `series`
 --
 ALTER TABLE `series`
-  MODIFY `ID_serie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `ID_serie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `usuarios`
